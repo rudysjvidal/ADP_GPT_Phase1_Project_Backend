91edbf93a8a6f9cafd8851412ad2c033f0c7121e
Rudys J Vidal <rvid1019@gmail.com>
2025-08-14 10:55:35 -0400

    test to see if render deploys

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { ObjectId } from 'mongodb';
-import { connectToDatabase, getCollection, getUserCollection } from './data-access.js';
+const { connectToDatabase, getCollection } = require('./data-access');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
